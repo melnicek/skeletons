bb322fb7b2e486594b287ff9d21a2d7b795b473c
Peter Melnicek <melnicek@outlook.com>
2020-06-18 16:59:42 +0200

    Update 0-htb.sh

--- 0-htb.sh
+++ 0-htb.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -e 1 ]
 then
-        echo "Usage: $0 <number-name> <ip>"
+        cd $1
+        tmux new -s htb -d
+        tmux send-keys -t htb:0 'openvpn /root/vpn/yelenz.ovpn' ENTER
+        tmux new-window
+        tmux send-keys -t htb:1 'ping -c 4 `cat ip`' ENTER
+        tmux attach -t htb
+elif [ $# -e 2 ]
+then
+        mkdir $1
+        cd $1
+        mkdir www loot info recon
+        echo $2 > ip
+        tmux new -s htb -d
+        tmux send-keys -t htb:0 'openvpn /root/vpn/yelenz.ovpn' ENTER
+        tmux new-window
+        tmux send-keys -t htb:1 'sleep 3; nmap -sC -sV -v -o recon/nmap `cat ip`; nmap -p- -sV -sC -v -o recon/nmap-full `cat ip`' ENTER
+        tmux attach -t htb
+else
+        echo "Usage:"
+        echo "\t create: $0 <number-name> <ip>"
+        echo "\t continue: $0 <number-name>"
         exit 1
 fi
-
-mkdir $1
-cd $1
-mkdir www loot info recon
-echo $2 > ip
-tmux new -s htb -d
-tmux send-keys -t htb:0 'openvpn /root/vpn/yelenz.ovpn' ENTER
-tmux new-window
-tmux send-keys -t htb:1 'sleep 3; nmap -sC -sV -v -o recon/nmap `cat ip`; nmap -p- -sV -sC -v -o recon/nmap-full `cat ip`' ENTER
-tmux attach -t htb
